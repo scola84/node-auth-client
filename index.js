9e1676087dc8d5d91822f6523745a4ce58a257ab
M.C. van den Boogaart <scola@kvdb.name>
2016-12-14 15:16:09 +0100

    Update dependency

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import targetRoute from './src/route/target';
 import loginRoute from './src/route/login';
 
-import { data } from '@scola/auth-common';
+import { loadI18n } from '@scola/auth-common';
 export { User } from '@scola/auth-common';
 
 export { default as authorize } from './src/filter/authorize';
@@ -9,7 +9,7 @@ export { default as logIn } from './src/helper/log-in';
 export { default as logOut } from './src/helper/log-out';
 
 export function authClient(router, connection, i18n) {
-  data(i18n);
+  loadI18n();
 
   targetRoute(router);
   loginRoute(router, connection, i18n);
