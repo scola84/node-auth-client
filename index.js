890e605ddae7531b147b527e16a7661ad56d8da6
M.C. van den Boogaart <scola@kvdb.name>
2016-12-15 23:43:17 +0100

    Fix function name

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ export { default as authorize } from './src/filter/authorize';
 export { default as logIn } from './src/helper/log-in';
 export { default as logOut } from './src/helper/log-out';
 
-export function authClient(router, connection, i18n) {
+export function client(router, connection, i18n) {
   loadI18n();
 
   targetRoute(router);
